940642cdc82b62b691ef715d1da3c62bb26c2f15
Satish Patil <satish.patil7017@gmail.com>
2022-11-04 10:02:02 +0530

    Added Palindrom Number Problem

---
+++ AssignmentProblems/AssignmentProblems/PalindromeNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentProblems
+{
+    internal class PalindromeNumber
+    {
+        public void Palindrom()
+        {
+            int n, r, sum = 0, temp;
+            Console.WriteLine("Enter the number :");
+            n = Convert.ToInt32(Console.ReadLine());
+            temp = n;
+            while (n > 0)
+            {
+                r = n % 10;
+                sum = (sum * 10) + r;
+                n = n / 10;
+            }
+            if (temp == sum)
+                Console.WriteLine("Number is Palindrom ");
+            else
+                Console.WriteLine("Number is not Palindrom");
+        }
+    }
+}
--- AssignmentProblems/AssignmentProblems/Program.cs
+++ AssignmentProblems/AssignmentProblems/Program.cs
@@ -8,15 +8,14 @@ namespace AssignmentProblems
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("3 for Reverse Number Problem");
+            Console.WriteLine("4 for Palindrom Number Problem");
             int Problem = Convert.ToInt32(Console.ReadLine());
             switch (Problem)
             {
-                case 3:
-                    ReverseNumber reverse = new ReverseNumber();
-                    reverse.Reverse();
+                case 4:
+                    PalindromeNumber palin = new PalindromeNumber();
+                    palin.Palindrom();
                     break;
-
             }
         }
     }
