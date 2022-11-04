a65338c98efb602a9db88cb709c0b4d9b98237a9
Satish Patil <satish.patil7017@gmail.com>
2022-11-04 09:45:44 +0530

    Added Fibonacci Series Problem

---
+++ AssignmentProblems/AssignmentProblems/Program.cs
@@ -0,0 +1,23 @@
+﻿using AssignmentProblems;
+using System;
+
+namespace AssignmentProblems
+
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("1 for Fibonacci Series Problem");
+            int Problem = Convert.ToInt32(Console.ReadLine());
+            switch (Problem)
+            {
+                case 1:
+                    FibonacciSeries fibonacci = new FibonacciSeries();
+                    fibonacci.Fibonaaci();
+                    break;
+
+            }
+        }
+    }
+}
